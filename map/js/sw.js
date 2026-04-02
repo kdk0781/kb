07ac350b42391057c0302221b380e8a045473e6f
kdk0781 <44281520+kdk0781@users.noreply.github.com>
2026-04-02 12:39:34 +0900

    Update sw.js

--- map/js/sw.js
+++ map/js/sw.js
@@ -1,1 +1,42 @@
+const CACHE_NAME = 'apt-price-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './css/common.css',
+  './js/app.js'
+];
 
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // 항상 최신 데이터를 불러오기 위한 Network-First 전략
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
